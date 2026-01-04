5ce7ee485343b9356127889f4c46a1b7ae98a582
kannathasansdet <kannathasansdet@gmail.com>
2026-01-04 20:51:22 +0530

    Abstraction - Abstract Class

---
+++ src/main/java/com/java/learning/abstractionDemo/Vehicle.java
@@ -0,0 +1,15 @@
+package com.java.learning.abstractionDemo;
+
+// Abstract class
+public abstract class Vehicle {
+
+    // Abstract method
+    abstract void start();
+
+    abstract void stop();
+
+    // Non-abstract method
+    void fuel() {
+        System.out.println("Vehicle uses fuel");
+    }
+}
